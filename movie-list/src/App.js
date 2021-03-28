ed3dc87f549697fedfe3cb70d3fca752fecc290a
Aaron Espinoza <aaronespinozz@gmail.com>
2021-03-28 20:35:06 +0200

    line-gradient to title and logo

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -30,8 +30,7 @@ class App extends React.Component{
 
     orderMovies(e) {
 
-      const orderYear = e.target.value;
-      const orderAlpha = e.target.value;
+      const {orderYear, orderAlpha} = e.target.value;
   
       if (orderYear === 'oldest') {
         this.setState({
@@ -44,16 +43,16 @@ class App extends React.Component{
           MoviesList: movies.sort((a, b) => b.year - a.year)
         })
       }
-      if (orderAlpha === 'A-Z') {
-        this.setState({
-          MoviesList: movies.sort((a, b) => b.title - a.title)
-        })
-      }
-      if (orderAlpha === 'Z-A') {
-        this.setState({
-          MoviesList: movies.sort((a, b) => b.title - a.title)
-        })
-      }
+      // if (orderAlpha === 'A-Z') {
+      //   this.setState({
+      //     MoviesList: movies.sort((a, b) => b.title - a.title)
+      //   })
+      // }
+      // if (orderAlpha === 'Z-A') {
+      //   this.setState({
+      //     MoviesList: movies.sort((a, b) => b.title - a.title)
+      //   })
+      // }
   
     }
     render(){
@@ -62,15 +61,18 @@ class App extends React.Component{
           
           <Header/>
           <div className='div-buttons'>
-          <button onClick={e=> this.fetch()}> fetch all Films!</button>
-          <button onClick={e=> this.reset()}> DELETE ALL FILMS</button>
-          <select onChange={e => this.orderMovies(e)} name="" id="">
-            <option value="">-choose</option>
+          <button className='fetch-color' onClick={e=> this.fetch()}> fetch all Films!</button>
+          <button onClick={e=> this.reset(e)}> DELETE ALL FILMS</button>
+          <select onChange={e => this.orderMovies(e)} name="Sort by:" id="">
             <option value="newest">NEWEST</option>
             <option value="oldest">oldest</option>
-            <option value="A-Z">A-Z</option>
+        </select>
+        <button onClick={e=> this.orderMovies(e)}></button>
+        <select onChange={e=> this.orderMovies(e)} name="" id="">
+          <option value="A-Z">A-Z</option>
             <option value="Z-A">Z-A</option>
         </select>
+
           </div>
           <MoviesList movies={this.state.MoviesList}/>
         </div>
